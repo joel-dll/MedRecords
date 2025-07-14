e77f5f8747b82ba3e86b2eef7b06ece595a69627
claudiamendes <1ribev81@solent.ac.uk>
2025-07-14 13:02:22 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className="video-container">
-      {/*backgroundvideo*/}
+      {/*background__svideo*/}
       <video autoPlay muted loop playsInline className="background-video">
         <source src="/backgroundVideo.mp4" type="video/mp4" />
       </video>
